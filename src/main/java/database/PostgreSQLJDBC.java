71d7983469b9d18ae4f5de3ac180534c3c9d5ae4
Szymon Niemiec <szniemiec@yahoo.com>
2020-05-25 15:09:48 +0200

    Hide database credentials

--- src/main/java/database/PostgreSQLJDBC.java
+++ src/main/java/database/PostgreSQLJDBC.java
@@ -6,18 +6,19 @@ import java.sql.SQLException;
 
 public class PostgreSQLJDBC {
 
-    private static final String HOST = "ec2-54-247-78-30.eu-west-1.compute.amazonaws.com";
-    private static final String PORT = "5432";
-    private static final String DATABASE = "d5r239i6ius89k";
-    private static final String LOGIN = "kjghikddzjahoq";
-    private static final String PASSWORD = "df9d199d66fc0d99e2e4d883234e9ed65e5f28b14222cff4c118ebe5c2d0cfa6";
     private Connection c;
 
     public PostgreSQLJDBC() {
         c = null;
     }
 
-    public void connectToDatabase() {
+    public void connectToDatabase(DatabaseCredentials databaseCredentials) {
+        String HOST = databaseCredentials.getHost();
+        String PORT = databaseCredentials.getPort();
+        String DATABASE = databaseCredentials.getDatabase();
+        String LOGIN = databaseCredentials.getLogin();
+        String PASSWORD = databaseCredentials.getPassword();
+
         try {
             c = DriverManager.getConnection("jdbc:postgresql://" + HOST + ":" + PORT + "/" + DATABASE, LOGIN, PASSWORD);
         } catch (Exception e) {
---
+++ src/main/java/services/Reader.java
@@ -0,0 +1,34 @@
+package services;
+
+import database.DatabaseCredentials;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Reader {
+
+    public Reader() {
+
+    }
+
+    public DatabaseCredentials readDatabaseEnviroment() {
+        DatabaseCredentials databaseCredentials = null;
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("enviroment.csv"))) {
+
+            String line = br.readLine();
+            String[] databaseEnviromentInfo = line.split(",");
+            databaseCredentials = new DatabaseCredentials(databaseEnviromentInfo[0],
+                    databaseEnviromentInfo[1],
+                    databaseEnviromentInfo[2],
+                    databaseEnviromentInfo[3],
+                    databaseEnviromentInfo[4]);
+
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+        return databaseCredentials;
+    }
+}
