e143a77e8b25dbc13b1b19ccc3b3772d274ddf0c
KozubIgn <ignacykozub@gmail.com>
2020-05-28 11:16:48 +0200

    add/ addNewQuest() & addNewArtifact()

--- src/main/java/controller/MentorController.java
+++ src/main/java/controller/MentorController.java
@@ -1,42 +1,42 @@
 package controller;
 
-import daos.artifact.ArtifactDAO;
 import daos.artifact.ArtifactDAOImpl;
-import daos.quest.QuestDAO;
 import daos.quest.QuestDAOImpl;
 import database.PostgreSQLJDBC;
 import models.users.Codecooler;
-import models.users.Mentor;
 
-import javax.swing.text.View;
+import view.View;
+
 import java.util.List;
 import java.util.Scanner;
 
 public class MentorController {
-    PostgreSQLJDBC postgreSQLJDBC;
+
+    private PostgreSQLJDBC postgreSQLJDBC;
     private QuestDAOImpl questDAO;
     private ArtifactDAOImpl artifactDAO;
-    private View view;
+    public View view;
     public List<Codecooler> codecoolers;
 
-    public MentorController(){
+    public MentorController() {
         view = new View();
         questDAO = new QuestDAOImpl(postgreSQLJDBC);
         artifactDAO = new ArtifactDAOImpl(postgreSQLJDBC);
-
     }
-    public MenuMentor(){
+
+    public void MenuMentor() {
         boolean isRunning = true;
-        while (isRunning){
+        while (isRunning) {
             view.clearScreen();
-            view.displayMnetorMenu();
+            view.displayMentorMenu();
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             switch (input) {
                 case "1":
+                    createNewQuest();
                     break;
-
                 case "2":
+                    createNewArtifact();
                     break;
 
                 case "3":
@@ -63,5 +63,31 @@ public class MentorController {
 
             }
         }
+    }
+
+    public void createNewQuest() {
+        System.out.println(view.NAME);
+        String name = view.getInput();
+        System.out.println(view.DESCRIPTION);
+        int description = view.getIntInput();
+        System.out.println(view.CATEGORY);
+        String category = view.getInput();
+        System.out.println(view.REWARD);
+        int reward = view.getIntInput();
+
+        questDAO.AddQuest(name, description, category, reward);
+        questDAO = new QuestDAOImpl(postgreSQLJDBC);
+    }
+
+    public void createNewArtifact() {
+        System.out.println(view.NAME);
+        String name = view.getInput();
+        System.out.println(view.DESCRIPTION);
+        int description = view.getIntInput();
+        System.out.println(view.REWARD);
+        int reward = view.getIntInput();
+
+        artifactDAO.AddArtifact(name, description, reward);
+        artifactDAO = new ArtifactDAOImpl(postgreSQLJDBC);
     }
 }
