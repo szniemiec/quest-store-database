c6dc6649133e0427b1521a1de3487e15deb6c05f
codewarior123 <przemek.buszek@onet.eu>
2020-06-23 16:02:07 +0200

    imports added

--- src/main/java/daos/codecooler/CodecoolerDAOImpl.java
+++ src/main/java/daos/codecooler/CodecoolerDAOImpl.java
@@ -1,16 +1,18 @@
 package daos.codecooler;
-
+import database.PostgreSQLJDBC;
+import enums.ModuleEnum;
+import enums.RoleEnum;
+import models.Purse;
+import models.users.AccountCredentials;
 import models.users.Codecooler;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
-
-public interface CodecoolerDAO {
-
-    public List<Codecooler> getCodecoolers() throws SQLException;
-
-    public Codecooler getCodecooler(int id) throws SQLException;
-
-    public void deleteCodecooler(int id);
-
-}
+public class CodecoolerDAOImpl implements CodecoolerDAO {
+    private PostgreSQLJDBC postgreSQLJDBC;
+    public CodecoolerDAOImpl(PostgreSQLJDBC postgreSQLJDBC) {
+        this.postgreSQLJDBC = postgreSQLJDBC;
+    }
