1823692148fbfc5d8cb128623a48a2342fe78e94
KozubIgn <ignacykozub@gmail.com>
2020-07-07 16:16:56 +0200

    delete @Override handle method

--- src/main/java/controller/MentorController.java
+++ src/main/java/controller/MentorController.java
@@ -22,7 +22,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class MentorController implements HttpHandler {
+public class MentorController {
     public InputService inputService;
     private Artifact artifact;
     private Quest quest;
@@ -278,30 +278,5 @@ public class MentorController implements HttpHandler {
             default -> throw new Exception("Wrong category id");
         }
         return questCategoryEnum;
-    }
-
-    @Override
-    public void handle(HttpExchange httpExchange) throws IOException {
-        String response = "";
-
-        try {
-            List<Mentor> mentors = this.mentorDAO.getMentors();
-            //tworzenie jsona
-            // response to to co sie poazuje na localost w przegladarce
-            ObjectMapper mapper = new ObjectMapper();
-            response = mapper.writeValueAsString(mentors);
-            System.out.println(response);
-
-            httpExchange.getResponseHeaders().put("Content-Type", Collections.singletonList("application/json"));
-            //CORS policy * - zezwolenie na komunikacje z kazdym frontem
-            httpExchange.getResponseHeaders().put("Access-Control-Allow-Origin", Collections.singletonList("*"));
-            httpExchange.sendResponseHeaders(200, response.getBytes().length);
-
-        }catch (Exception e) {
-            httpExchange.sendResponseHeaders(404, response.length());
-        }
-        OutputStream os = httpExchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
     }
 }
