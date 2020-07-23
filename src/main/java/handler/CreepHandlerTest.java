abd8174b58f250b06784b8d877ef4e0311bdb46b
KozubIgn <ignacykozub@gmail.com>
2020-07-23 18:05:04 +0200

    Add CreepHandler test

---
+++ src/main/java/handler/CreepHandlerTest.java
@@ -0,0 +1,56 @@
+package handler;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import daos.mentor.MentorDAOImpl;
+import database.DatabaseCredentials;
+import database.PostgreSQLJDBC;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import services.JSONService;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+class CreepHandlerTest {
+
+    PostgreSQLJDBC postgreSQLJDBC = new PostgreSQLJDBC();
+    CreepHandler creepHandler = new CreepHandler(postgreSQLJDBC);
+
+    @BeforeEach
+    void prepareTest() {
+        JSONService jsonService = new JSONService();
+        DatabaseCredentials credentials = jsonService.readEnviroment();
+        postgreSQLJDBC.connectToDatabase(credentials);
+        ///
+
+    }
+    @Test
+    void handle() throws Exception {
+        HttpExchange httpExchangeMock = Mockito.mock(HttpExchange.class);
+        MentorDAOImpl mentorDaoMock = Mockito.mock(MentorDAOImpl.class);
+        Mockito.when(httpExchangeMock.getRequestURI()).thenReturn(URI.create("/creep/mentors"));
+        Mockito.when(mentorDaoMock.getMentors()).thenReturn(new ArrayList<>());
+        Mockito.when(httpExchangeMock.getResponseHeaders()).thenReturn(new Headers());
+        Mockito.when(httpExchangeMock.getResponseBody()).thenReturn(new ByteArrayOutputStream(180));
+        String expected = "{\"id\":23,\"accountCredentials\":{\"login\":\"adrian\",\"password\":" +
+                "\"c23ad6f18412014673b2d04794ca038ef6767fe94afe408dffb775362fe07e68\"," +
+                "\"email\":\"adrian@gmail.com\",\"roleEnum\":\"MENTOR\"},\"firstName\":\"Adrian\",\"lastName\":\"Adrian\"}";
+        creepHandler.handle(httpExchangeMock);
+//        Assertions.assertEquals(expected, creepHandler.getResponse());
+        Assertions.assertNotNull(creepHandler.getResponse());
+    }
+    @AfterEach
+    void closeTest() {
+        try {
+            postgreSQLJDBC.disconnectFromDatabase();
+        } catch (SQLException ignore) {
+        }
+    }
+}
