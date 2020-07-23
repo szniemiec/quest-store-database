f3e031e0054c0079cde5de31f31b6bb39a71b364
KozubIgn <ignacykozub@gmail.com>
2020-07-23 10:51:26 +0200

    add method getRoleId()

--- src/main/java/enums/RoleEnum.java
+++ src/main/java/enums/RoleEnum.java
@@ -6,7 +6,13 @@ public enum RoleEnum {
     MENTOR(2),
     CODECOOLER(3);
 
+    private final int roleId;
+
     RoleEnum(int roleId) {
+        this.roleId = roleId;
     }
 
+    public int getRoleId() {
+        return roleId;
+    }
 }
